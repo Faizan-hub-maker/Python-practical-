6d9b5cbdc5a6402f747e37d2e8b61bf9e231e06f
Faizan-hub-maker <faijanshaikh@eng.rizvi.edu.in>
2026-02-17 09:31:30 +0530

    Exp4.py

---
+++ Exp4.py
@@ -0,0 +1,7 @@
+P=float(input("Enter your Principle Amount(in Rupees):Rs"))
+R=float(input("Enter your Rate of Interest(per Year):"))
+T=float(input("Enter your Time Period(in Years):"))
+S=(P*R*T)/100
+print(f"Simple Interest=Rs",S)
+print("UIN=251A029")
+print("Date:27-01-2026")
