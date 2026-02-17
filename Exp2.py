d4df74d711e3f2090e874acc631da13e72dd9ef0
Faizan-hub-maker <faijanshaikh@eng.rizvi.edu.in>
2026-02-17 09:30:22 +0530

    Exp2.py

---
+++ Exp2.py
@@ -0,0 +1,16 @@
+print("Calculaton of Area of Circle")
+radius=float(input("Emter the radius:"))
+print(f"Your radius is {radius}")
+area=3.142*radius*radius
+print(f"Area of circle={area}")
+print("Calculation of Area of Rectangle")
+length=float(input("Enter the length:"))
+breadth=float(input("Enter the breadth:"))
+area=length*breadth
+print(f"Area of Rectangle={area}")
+print("Calculation of Area of Square")
+side=float(input("Enter the side:"))
+area=side*side
+print(f"Area of Square={area}")
+print("UIN:251A029")
+print("Date:20-01-2026")
