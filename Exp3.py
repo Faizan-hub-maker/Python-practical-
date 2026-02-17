abb1b22198fe327728ea91fca3587979b3d66c90
Faizan-hub-maker <faijanshaikh@eng.rizvi.edu.in>
2026-02-17 09:31:00 +0530

    Exp3.py

---
+++ Exp3.py
@@ -0,0 +1,12 @@
+import math
+BS=int(input("Enter your Basic Salary:Rs"))
+DA=(BS*70)/100
+TA=(BS*30)/100
+HRA=(BS*10)/100
+GS=(BS+DA+TA+HRA)
+print("Dearness Allowance=Rs",DA)
+print("Travel Allowance=Rs",TA)
+print("House Rental Allowance=Rs",HRA)
+print("Gross Salary=Rs",GS)
+print("UIN=251A029")
+print("Date:27-01-2026")
